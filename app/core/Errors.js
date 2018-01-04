3d6d6d797a4ffcd2a83c9fc7d8fca60b835a1f46
satyashani <satyashani@gmail.com>
2018-01-04 12:49:13 +0530

    Base model update

--- app/core/Errors.js
+++ app/core/Errors.js
@@ -7,7 +7,9 @@
 var errors = {
     'no_search_condition' : "No search condition given",
     'user_exists' : 'User already exists',
-    'invalid_userid' : "Invalid user id input"
+    'invalid_userid' : "Invalid user id input",
+    'invalid_field_name' : "Invalid field name in sql",
+    'invalid_sql_matcher' : "Invalid conditional syntax in sql"
 };
 
 module.exports = errors;
